244770efc486517b40089e48c2782ed7585f769a
Alexander_Ovchar <azalexxx02@gmail.com>
2024-08-03 15:18:32 +0300

    init three

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {OrbitControls} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
 import React from "react";
 
 function App() {
-  return <div >123</div>;
+  return (
+    <div className="h-screen">
+      <Canvas>
+        <OrbitControls />
+        <ambientLight />
+        <pointLight position={[3, 2, 3]} intensity={10} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
